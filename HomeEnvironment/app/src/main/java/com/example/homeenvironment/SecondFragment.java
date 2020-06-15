3e1b45b7c1e697ac7ff2d2fc70838fa0a9d12238
s194579 <s194579@student.dtu.dk>
2020-06-15 16:42:04 +0200

    Ups skal ikke arbejde her

--- HomeEnvironment/app/src/main/java/com/example/homeenvironment/SecondFragment.java
+++ HomeEnvironment/app/src/main/java/com/example/homeenvironment/SecondFragment.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SecondFragment extends Fragment {
+
+    private long tipID;
+
+    public void setTipID(long tipID){
+        this.tipID = tipID;
+    }
 
     @Override
     public View onCreateView(
@@ -23,12 +31,15 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if(view != null){
+
+        }
     }
 }
