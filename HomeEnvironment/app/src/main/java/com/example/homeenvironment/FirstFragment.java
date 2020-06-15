be0c6334076caa1b9b4237e697c1fb25d660fada
danielschutt <59561137+danielschutt@users.noreply.github.com>
2020-06-15 15:54:13 +0200

    Fungerende lys sensor

--- HomeEnvironment/app/src/main/java/com/example/homeenvironment/FirstFragment.java
+++ HomeEnvironment/app/src/main/java/com/example/homeenvironment/FirstFragment.java
@@ -1,15 +1,32 @@
 package com.example.homeenvironment;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import static android.content.Context.SENSOR_SERVICE;
+import static android.hardware.Sensor.TYPE_LIGHT;
+
 public class FirstFragment extends Fragment {
+    private appLightSensor mLightSensor;
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private SensorEventListener lightEventListener;
+    private float maxValue;
+    private float lightQuantity;
+    TextView luxText;
 
     @Override
     public View onCreateView(
@@ -22,12 +39,48 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        sensorManager = (SensorManager) view.getContext().getSystemService(SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(TYPE_LIGHT);
+        luxText = (TextView) view.findViewById(R.id.Lux_Measurement);
+
 
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+
+        lightEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                Toast.makeText(getContext(), "sensor changed", Toast.LENGTH_SHORT).show();
+                lightQuantity = sensorEvent.values[0];
+            }
+
+            //Denne funktion skal være der for SensorEventListeners
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        sensorManager.registerListener(lightEventListener, lightSensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+        view.findViewById(R.id.MeasuringLightButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (lightSensor == null) {
+                    Toast.makeText(view.getContext(), "This phone doesn't have a lightsensor", Toast.LENGTH_SHORT).show();
+                } else {
+                    luxText.setText(" This is the current lux " + lightQuantity);
+                }
+
+                //luxText.getText();
+                //Log.i("MainActivity","This is the text of the textview " + luxText.getText());
+                //luxText.setText("This is the current lux " + lightQuantity);
+
             }
         });
     }
---
+++ HomeEnvironment/app/src/main/java/com/example/homeenvironment/appLightSensor.java
@@ -0,0 +1,54 @@
+package com.example.homeenvironment;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.content.Context.SENSOR_SERVICE;
+import static android.hardware.Sensor.TYPE_LIGHT;
+
+public class appLightSensor {
+    private SensorManager sensorManager;
+    private Sensor lightSensor;
+    private SensorEventListener lightEventListener;
+    private float maxValue;
+    private float lightQuantity;
+    private View view;
+    TextView luxText;
+
+    public appLightSensor(View view){
+        sensorManager = (SensorManager) view.getContext().getSystemService(SENSOR_SERVICE);
+        this.view = view;
+        lightSensor = sensorManager.getDefaultSensor(TYPE_LIGHT);
+        luxText = (TextView) view.findViewById(R.id.Lux_Measurement);
+
+        lightEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                lightQuantity = sensorEvent.values[0];
+            }
+
+            //Denne funktion skal være der for SensorEventListeners
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+        sensorManager.registerListener(lightEventListener, lightSensor, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+    public float getLux(){
+        if(lightSensor == null) Toast.makeText(view.getContext(), "This phone doesn't have a lightsensor", Toast.LENGTH_SHORT).show();
+        return lightQuantity;
+
+
+    }
+
+
+}
+
+
