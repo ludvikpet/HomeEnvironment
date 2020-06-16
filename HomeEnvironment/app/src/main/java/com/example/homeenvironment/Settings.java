f14aa0f446d6d7c63b57f631622b098a53b34647
danielschutt <59561137+danielschutt@users.noreply.github.com>
2020-06-16 17:07:48 +0200

    Workingswitches

--- HomeEnvironment/app/src/main/java/com/example/homeenvironment/Settings.java
+++ HomeEnvironment/app/src/main/java/com/example/homeenvironment/Settings.java
@@ -2,9 +2,12 @@ package com.example.homeenvironment;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.SwitchPreference;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,7 +17,7 @@ import androidx.preference.PreferenceManager;
 public class Settings extends PreferenceActivity {
 
     public static SwitchPreference testPref;
-
+    private final static String TAG = "Settings";
     SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
         public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
             // Implementation
@@ -38,25 +41,31 @@ public class Settings extends PreferenceActivity {
             addPreferencesFromResource(R.xml.root_preferences);
 
             testPref = (android.preference.SwitchPreference) findPreference("test");
+            testPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener(){
+
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    if(testPref.isChecked()){
+                        Toast.makeText(getView().getContext(), "That worked! it's on!", Toast.LENGTH_SHORT);
+                        testPref.setChecked(false);
+                        Log.i(TAG, "that's OFF");
+                    }else if(!testPref.isChecked()){
+                        Log.i(TAG, "That's ON");
+                        Toast.makeText(getView().getContext(), "That worked! it's off!", Toast.LENGTH_SHORT);
+                        testPref.setChecked(true);
+                    }
+                    return false;
+                }
+            });
         }
     }
 
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals("test")) {
-            boolean test = sharedPreferences.getBoolean("test", false);
-            //Do whatever you want here. This is an example.
-            if (test) {
-                testPref.setSummary("Enabled");
-            } else {
-                testPref.setSummary("Disabled");
-            }
-        }
-    }
+
 
         @Override
         public void onResume () {
             super.onResume();
-
+/**
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(Settings.this);
             boolean test = preferences.getBoolean("test", false);
 
@@ -65,5 +74,6 @@ public class Settings extends PreferenceActivity {
             } else {
                 testPref.setSummary("Disabled");
             }
+ */
         }
     }
