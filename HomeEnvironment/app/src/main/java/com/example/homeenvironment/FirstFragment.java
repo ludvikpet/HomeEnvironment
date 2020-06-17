2d5c0d6f08ee51011259d8f67109f422fc01c9a6
Arcspider <sorenfw13@hotmail.com>
2020-06-17 17:10:40 +0200

    Layout scaling working
    Crash when app gets resumed after the back or home button has been used

--- HomeEnvironment/app/src/main/java/com/example/homeenvironment/FirstFragment.java
+++ HomeEnvironment/app/src/main/java/com/example/homeenvironment/FirstFragment.java
@@ -4,22 +4,25 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.hardware.SensorEventListener;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
-import androidx.core.widget.TextViewCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.homeenvironment.Sensors.AppBarometerSensor;
 import com.example.homeenvironment.Sensors.AppLightSensor;
 import com.example.homeenvironment.Sensors.AppTemperatureSensor;
-
-import java.util.Objects;
+import com.example.homeenvironment.Sensors.NoiseLevel;
 
 public class FirstFragment extends Fragment {
     private static final int MY_PERMISSIONS_RECORD_AUDIO = 1;
@@ -39,6 +42,7 @@ public class FirstFragment extends Fragment {
     ) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
+
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -47,6 +51,7 @@ public class FirstFragment extends Fragment {
         mLightSensor = new AppLightSensor(view);
         mBarometerSensor = new AppBarometerSensor(view);
         mTemperatureSensor = new AppTemperatureSensor(view);
+        final AlarmCreateActivity alarmCreateActivity = new AlarmCreateActivity(view);
         //luxText = view.findViewById(R.id.Lux_Measurement);
         //pressureText = view.findViewById(R.id.pressureSensorView);
         //humidityText = view.findViewById(R.id.Humidity_Text);
@@ -55,12 +60,15 @@ public class FirstFragment extends Fragment {
         pressureText = view.findViewById(R.id.pressureID);
         humidityText = view.findViewById(R.id.humidityID);
         temperatureText = view.findViewById(R.id.temperatureID);
-        final AlarmCreateActivity alarmCreateActivity = new AlarmCreateActivity(view);
         noiseLevelText = view.findViewById(R.id.noiseID);
+
+        setScale(view);
+
         if (ContextCompat.checkSelfPermission(this.requireContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_DENIED) {
             noiseLevel = new NoiseLevel(view);
-            noiseLevel.startRecorder();
+            if(!noiseLevel.isRunning()) noiseLevel.startRecorder();
         }
+
         setInfo();
 
         view.findViewById(R.id.tipsButton).setOnClickListener(new View.OnClickListener() {
@@ -75,6 +83,7 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
 
 //        view.findViewById(R.id.button_noiselevel).setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -99,12 +108,53 @@ public class FirstFragment extends Fragment {
         });
     }
 
+
     private void setInfo() {
         luxText.setText(getString(R.string.lightLevelInfo, 0));
         pressureText.setText(getString(R.string.pressureInfo, 0));
         humidityText.setText(getString(R.string.humidityInfo, 0));
         temperatureText.setText(getString(R.string.tempInfo, 0.0, "℃"));
         noiseLevelText.setText(getString(R.string.noiseInfo, 0.0));
+    }
+
+    private void setScale(View view){
+        int width = getResources().getConfiguration().screenWidthDp;
+        int height = getResources().getConfiguration().screenHeightDp;
+        float heightProduction = 800;
+        float widthProduction = 480;
+        int calculatedWidth = (int) (width * (100/ widthProduction));
+        Log.i("TAG", "Width: " +calculatedWidth);
+        float textSize = height * (25 / heightProduction) ;
+        Log.i("TAG", "TextSize " + height*(25/heightProduction));
+        TextView luxTitle, pressureTitle, humidityTitle, temperatureTitle, noiseLevelTitle;
+
+        luxTitle = view.findViewById(R.id.lightTextView);
+        pressureTitle = view.findViewById(R.id.pressureTextView);
+        humidityTitle = view.findViewById(R.id.humidityTextView);
+        temperatureTitle = view.findViewById(R.id.tempTextView);
+        noiseLevelTitle = view.findViewById(R.id.noiseTextView);
+
+        Button measureButton = view.findViewById(R.id.measureButton);
+        Button tipsButton = view.findViewById(R.id.tipsButton);
+        measureButton.setTextSize(textSize);
+        tipsButton.setTextSize(textSize);
+        setTitles(luxTitle, pressureTitle, humidityTitle, temperatureTitle, noiseLevelTitle, textSize, calculatedWidth);
+    }
+    private void setTitles(TextView luxTitle, TextView pressureTitle, TextView humidityTitle, TextView temperatureTitle, TextView noiseLevelTitle, float textSize, int width){
+        luxTitle.setTextSize(textSize);
+        pressureTitle.setTextSize(textSize);
+        humidityTitle.setTextSize(textSize);
+        temperatureTitle.setTextSize(textSize);
+        noiseLevelTitle.setTextSize(textSize);
+        setDimensions(luxText, textSize, width);
+        setDimensions(pressureText, textSize, width);
+        setDimensions(humidityText, textSize, width);
+        setDimensions(temperatureText, textSize, width);
+        setDimensions(noiseLevelText, textSize, width);
+    }
+    private void setDimensions(TextView textView, float textSize, int width){
+        textView.setTextSize(textSize);
+        textView.setWidth(width);
     }
 
 }
--- HomeEnvironment/app/src/main/java/com/example/homeenvironment/MainActivity.java
+++ HomeEnvironment/app/src/main/java/com/example/homeenvironment/MainActivity.java
@@ -13,12 +13,15 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_RECORD_AUDIO = 1;
