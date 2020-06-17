b5e41e840539ce345c07b27f41c4fa96a32a3645
s194608 <63788869+s194608@users.noreply.github.com>
2020-06-17 16:16:53 +0200

    SETTINGS OPDATERING

--- HomeEnvironment/app/src/main/java/com/example/homeenvironment/Settings.java
+++ HomeEnvironment/app/src/main/java/com/example/homeenvironment/Settings.java
@@ -9,6 +9,12 @@ import android.preference.SwitchPreference;
 import android.util.Log;
 import android.widget.Toast;
 
+<<<<<<< Updated upstream
+=======
+import com.example.homeenvironment.Sensors.AppTemperatureSensor;
+
+
+>>>>>>> Stashed changes
 public class Settings extends PreferenceActivity {
 
     public static SwitchPreference tempPref;
@@ -41,16 +47,25 @@ public class Settings extends PreferenceActivity {
                 @Override
                 public boolean onPreferenceChange(Preference preference, Object o) {
                     //Temp switch
+                    String tempBoolean = getString(R.string.tempBoolean);
 
                     if (tempPref.isChecked()) {
                         Toast.makeText(getView().getContext(), "That worked! it's on!", Toast.LENGTH_SHORT);
                         tempPref.setChecked(false);
+<<<<<<< Updated upstream
 
                         Log.i(TAG, "that's OFF");
                     } else if (!tempPref.isChecked()) {
                         Log.i(TAG, "That's ON");
                         Toast.makeText(getView().getContext(), "That worked! it's off!", Toast.LENGTH_SHORT);
                         tempPref.setChecked(true);
+=======
+                        tempBoolean = "false";
+
+                    } else {
+                        tempPref.setChecked(true);
+                        tempBoolean = "true";
+>>>>>>> Stashed changes
                     }
 
                     return false;
@@ -58,17 +73,26 @@ public class Settings extends PreferenceActivity {
             });
             //Notifications switch
             notificationsPref = (android.preference.SwitchPreference) findPreference("notifications");
-            notificationsPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            notificationsPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener()
+            {
                 @Override
                 public boolean onPreferenceChange(Preference preference, Object o) {
-
+                    String notificationBoolean = getString(R.string.notiBoolean);
                     if (notificationsPref.isChecked()) {
+<<<<<<< Updated upstream
                         Toast.makeText(getView().getContext(), "That worked! it's on!", Toast.LENGTH_SHORT);
+=======
+                        notificationBoolean = "false";
+>>>>>>> Stashed changes
                         notificationsPref.setChecked(false);
                         Log.i(TAG, "that's OFF");
                     } else if (!notificationsPref.isChecked()) {
+<<<<<<< Updated upstream
                         Log.i(TAG, "That's ON");
                         Toast.makeText(getView().getContext(), "That worked! it's off!", Toast.LENGTH_SHORT);
+=======
+                        notificationBoolean = "true";
+>>>>>>> Stashed changes
                         notificationsPref.setChecked(true);
                     }
                     return false;
