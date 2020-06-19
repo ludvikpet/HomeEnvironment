3ead024721a50483efad9ec54fbf6b196c0ae65f
Ludvík Petersen <s194613@student.dtu.dk>
2020-06-19 10:29:32 +0200

    Preference and list config

--- HomeEnvironment/app/src/main/java/com/example/homeenvironment/AlarmCreateActivity.java
+++ HomeEnvironment/app/src/main/java/com/example/homeenvironment/AlarmCreateActivity.java
@@ -32,20 +32,55 @@ public class AlarmCreateActivity{
                 if(s.equals("notifications")){
                     if(sharedPreferences.getBoolean(s,false)){
                         startAlarmNotification();
+                        Log.i("Alarm", "Restarted alarm");
                     }else{
+                        Log.i("Alarm", "Cancelled alarm");
                         cancelAlarmNotification();
                     }
                 }
 
             }
         });
+
+
     }
    
 
     private void startAlarmNotification(){
         alarmManager =(AlarmManager) view.getContext().getSystemService(Context.ALARM_SERVICE);
-        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),AlarmManager.INTERVAL_FIFTEEN_MINUTES,receiverPendingIntent);
-        Log.i("Alarm", "AlarmCreateActivity her! has set repeating ");
+
+        if (sharedPreferences.getString("time_interval", "halv time").equals("30 min")) {
+            alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), receiverPendingIntent);
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),AlarmManager.INTERVAL_HALF_HOUR / 120,receiverPendingIntent);
+            Log.i("Alarm", "AlarmCreateActivity her! has set repeating ----> 0.5t");
+        }
+        else if(sharedPreferences.getString("time_interval", "halv time").equals("60 min")) {
+            alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), receiverPendingIntent);
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),AlarmManager.INTERVAL_HOUR / 60,receiverPendingIntent);
+            Log.i("Alarm", "AlarmCreateActivity her! has set repeating ----> 1t");
+        }
+        else if(sharedPreferences.getString("time_interval", "halv time").equals("120 min")) {
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),AlarmManager.INTERVAL_HOUR * 2,receiverPendingIntent);
+            Log.i("Alarm", "AlarmCreateActivity her! has set repeating ----> 2t");
+        }
+        else if(sharedPreferences.getString("time_interval", "halv time").equals("180 min")) {
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),AlarmManager.INTERVAL_HOUR * 3,receiverPendingIntent);
+            Log.i("Alarm", "AlarmCreateActivity her! has set repeating ----> 3t");
+        }
+        else if(sharedPreferences.getString("time_interval", "halv time").equals("240 min")) {
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),AlarmManager.INTERVAL_HOUR * 4,receiverPendingIntent);
+            Log.i("Alarm", "AlarmCreateActivity her! has set repeating ----> 4t");
+        }
+        else if(sharedPreferences.getString("time_interval", "halv time").equals("300 min")) {
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),AlarmManager.INTERVAL_HOUR * 5,receiverPendingIntent);
+            Log.i("Alarm", "AlarmCreateActivity her! has set repeating ----> 5t");
+        }
+        else if(sharedPreferences.getString("time_interval", "halv time").equals("1440 min")) {
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),AlarmManager.INTERVAL_DAY,receiverPendingIntent);
+            Log.i("Alarm", "AlarmCreateActivity her! has set repeating ----> 24t");
+        }
+
+        Log.i("Alarm", "AlarmCreateActivity her! has set repeating ----->" + sharedPreferences.getString("time_interval", "halv time"));
     }
     private void cancelAlarmNotification(){
         if(alarmManager != null){
